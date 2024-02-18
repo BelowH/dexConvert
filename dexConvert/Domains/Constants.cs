cf01c3cc23aee91a862d886f05193c5c0463b7ff
Christoph Belohaubek <66495513+BelowH@users.noreply.github.com>
2024-02-18 22:20:33 +0100

    cors proxy

--- dexConvert/Domains/Constants.cs
+++ dexConvert/Domains/Constants.cs
@@ -2,10 +2,10 @@ namespace dexConvert.Domains;
 
 public static class Constants
 {
-    public const string BaseUrl = "https://api.mangadex.org";
+    public const string BaseUrl = "https://corsproxy.io/?https://api.mangadex.org";
 
-    public const string CoverBaseUrl = "https://uploads.mangadex.org";
+    public const string CoverBaseUrl = "https://corsproxy.io/?https://uploads.mangadex.org";
 
-    public const string ReportBaseUrl = "https://api.mangadex.network";
+    public const string ReportBaseUrl = "https://corsproxy.io/?https://api.mangadex.network";
 
 }
